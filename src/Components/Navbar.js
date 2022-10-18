ad3ce57576f0caf233c4cdab0ac9f3ec9981049d
Heph-zibah <oluwatosinhephzibah@gmail.com>
2022-10-18 08:34:11 +0100

    navbar markup

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -2,16 +2,19 @@ import React from 'react';
 
 const Navbar = () => {
 	return (
-		<div className='font-Roboto'>
-			<div>
-				<div>
-					<h1 className=''>
-						Oluwalogbon <span>Electronics</span>
-					</h1>
+		<div className='font-Roboto  pt-[36px] flex justify-center'>
+			<div className='w-full max-w-[1024px]'>
+				<div className='flex'>
+					<div>
+						<h1 className='text-black font-bold text-xl'>
+							Oluwalogbon{' '}
+							<span className='text-[#626161] font-normal'>Electronics</span>
+						</h1>
+					</div>
+					<form action=''>
+						<input type='text' name='categories' placeholder='ALL CATEGORIES' />
+					</form>
 				</div>
-				<form action=''>
-					<input type='text' name='categories' placeholder='ALL CATEGORIES' />
-				</form>
 			</div>
 		</div>
 	);
